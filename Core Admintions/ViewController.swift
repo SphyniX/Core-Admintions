7f986ef39bd8b2b5c88d8f4488d1e7dfe9118fde
焦俊豪 <jiaojunhao1@www.gz.10086.cn>
2015-01-14 19:08:17 +0800

    Initial Commit

---
+++ Core Admintions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core Admintions
+//
+//  Created by 焦俊豪 on 15/1/14.
+//  Copyright (c) 2015年 焦俊豪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
